00de2faa4c27b8cf28269389203c235cd7afcc3a
xujie <xujie@huiyu.org.cn>
2019-12-24 17:09:18 +0800

    修改数据库连接  localhost--->阿里云服务器

---
+++ src/main/java/top/xujie/tools/entity/Student.java
@@ -0,0 +1,37 @@
+package top.xujie.tools.entity;
+
+import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author xujie
+ * @since 2019-12-24
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+public class Student implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    private String phone;
+
+    private String mobileCity;
+
+    private String address;
+
+    private String birthday;
+
+}
---
+++ src/main/java/top/xujie/tools/mapper/StudentMapper.java
@@ -0,0 +1,16 @@
+package top.xujie.tools.mapper;
+
+import top.xujie.tools.entity.Student;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author xujie
+ * @since 2019-12-24
+ */
+public interface StudentMapper extends BaseMapper<Student> {
+
+}
